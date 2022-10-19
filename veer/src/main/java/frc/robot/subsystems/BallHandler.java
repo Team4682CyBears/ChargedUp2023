3b999032cdb02266673f4038a42bc6dede4ff43f
Mike McAdams <56608471+mmcadamsms@users.noreply.github.com>
2022-10-18 17:21:25 -0700

    First block of code

---
+++ veer/src/main/java/frc/robot/subsystems/BallHandler.java
@@ -0,0 +1,87 @@
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - Rapid React - 2022
+// File: BallHandler.java
+// Intent: Wrapper class standard stub for robot in FRC challange.
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.common.*;
+
+import static frc.robot.Constants.*;
+
+public class BallHandler extends SubsystemBase implements Sendable {
+
+    private WPI_TalonSRX ballMotor = new WPI_TalonSRX(Constants.BallHandlerMotorCanId);
+    private boolean currentBallArmDeployed = false;
+
+    /**
+     * No argument constructor for the BallHandler subsystem.
+    */
+    public BallHandler() {
+    }
+
+    /**
+     * 
+     */
+    public void deployPosition(){
+
+    }
+
+    public void retractPosition(){
+
+    }
+
+    public void togglePosition(){
+
+    }
+
+    public void storeBall(){
+
+    }
+
+    public void retrieveBall(){
+
+    }
+
+    public void stopStoringBall(){
+
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder)
+    {
+      builder.addDoubleProperty("BallMotorSpeed", this::getBallMotorSpeed, null);
+      builder.addStringProperty("BallArmState", this::getBallArmState, null);
+    }
+ 
+    private String getBallArmState()
+    {
+        return (this.currentBallArmDeployed ? "Arm Deployed" : "Arm Retracted");
+    }
+
+    private double getBallMotorSpeed()
+    {
+        return this.ballMotor.get();
+    }
+
+    private void intitalizeBallArmState()
+    {
+
+    }
+
+}
