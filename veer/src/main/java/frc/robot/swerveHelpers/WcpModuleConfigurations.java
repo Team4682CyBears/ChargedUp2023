aff07f507fc01b7b3b60427deab455d1bde8c1a0
josieammer <josieammer@gmail.com>
2022-10-23 14:27:52 -0700

    adding swerveX gear ratios from Kyle
    adding swerveX gear ratios from Kyle

--- veer/src/main/java/frc/robot/swerveHelpers/WcpModuleConfigurations.java
+++ veer/src/main/java/frc/robot/swerveHelpers/WcpModuleConfigurations.java
@@ -9,10 +9,10 @@ import com.swervedrivespecialties.swervelib.ModuleConfiguration;
 
 public class WcpModuleConfigurations {
     public static final ModuleConfiguration SWERVEX = new ModuleConfiguration(
-            0.1016,  //DONE
-            (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 60.0), //TODO
-            true, //TODO
-            (15.0 / 32.0) * (10.0 / 60.0), //TODO
-            true //TODO
+            0.1016,  //4" in meters
+            1/7.85, // 7.85:1 (10:34 -> 26:20 -> 15:45)
+            true, //three reductions
+            1/15.43, // 15.43:1 (8:24 -> 14:72)
+            true //two reductions
     );
 }
