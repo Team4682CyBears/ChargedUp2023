bacfaf5d783258dc98f00387ade062fb8ae48c43
Mike McAdams <56608471+mmcadamsms@users.noreply.github.com>
2022-10-25 18:04:08 -0700

    Update to try and hook in can coder default settings

--- veer/src/main/java/frc/robot/Robot.java
+++ veer/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.swerveHelpers.CtreSettings;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -29,6 +30,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // hack to update CTRE can coder settings
+    CtreSettings.UpdateCanEncoderDefaultSettings();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
---
+++ veer/src/main/java/frc/robot/swerveHelpers/CtreSettings.java
@@ -0,0 +1,32 @@
+package frc.robot.swerveHelpers;
+
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.swervedrivespecialties.swervelib.ctre.CanCoderAbsoluteConfiguration;
+
+import frc.robot.Constants;
+
+public class CtreSettings {
+
+    public static void UpdateCanEncoderDefaultSettings()
+    {
+        CANCoder backLeftCoder = new CANCoder(Constants.BACK_LEFT_MODULE_STEER_ENCODER);
+        CANCoder backRightCoder = new CANCoder(Constants.BACK_RIGHT_MODULE_STEER_ENCODER);
+        CANCoder frontLeftCoder = new CANCoder(Constants.FRONT_LEFT_MODULE_STEER_ENCODER);
+        CANCoder frontRightCoder = new CANCoder(Constants.FRONT_RIGHT_MODULE_STEER_ENCODER);
+
+        CtreSettings.UpdateSingleCanEncoderDefaultSettings(backLeftCoder);
+        CtreSettings.UpdateSingleCanEncoderDefaultSettings(backRightCoder);
+        CtreSettings.UpdateSingleCanEncoderDefaultSettings(frontLeftCoder);
+        CtreSettings.UpdateSingleCanEncoderDefaultSettings(frontRightCoder);
+    }
+    
+    private static void UpdateSingleCanEncoderDefaultSettings(CANCoder cancoder)
+    {
+        CANCoderConfiguration config = new CANCoderConfiguration();
+        SensorInitializationStrategy strat = SensorInitializationStrategy.BootToZero;
+        config.initializationStrategy = strat;
+        cancoder.configAllSettings(config);
+    }
+}
