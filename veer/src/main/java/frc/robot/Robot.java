da5af79a74935ca3cd2f0b5e7639076320c1f2f4
josieammer <josieammer@gmail.com>
2022-10-26 19:47:49 -0700

    Revert "Updates to attempt to zero all swerve steer motor encoders"
    This reverts commit ef0da5ebccbc3d3df5592d9452f170eb7fa47d22.

--- veer/src/main/java/frc/robot/Robot.java
+++ veer/src/main/java/frc/robot/Robot.java
@@ -31,12 +31,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // TODO - hack to update CTRE can coder settings    
+    // hack to update CTRE can coder settings
     //CtreSettings.UpdateCanEncoderDefaultSettings();
-
-    // TODO - hack to update CTRE talon motor encoder setting
-    CtreSettings.ZeroAllSwerveSteerMotorEncoders();
-
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
