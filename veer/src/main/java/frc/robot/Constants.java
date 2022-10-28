b41773232248700c0fe2ebec21230d0e413b9e7f
Mike McAdams <56608471+mmcadamsms@users.noreply.github.com>
2022-10-27 18:24:20 -0700

    Updates to get ready for telescoping arm to be added to veer

--- veer/src/main/java/frc/robot/Constants.java
+++ veer/src/main/java/frc/robot/Constants.java
@@ -58,6 +58,17 @@ public final class Constants {
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 0; // FIXME Set back right steer encoder ID
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back right steer offset
 
+    // *****************************************************************
+    // standard stuff constants - motors rotation, etc.
+    public static final double DegreesPerRevolution = 360.0;
+    // NEO maximum RPM 
+    public static final double neoMaximumRevolutionsPerMinute = 5676;
+    // this uses the halls effect sensor when plugged into the spark max
+    // see: https://www.revrobotics.com/rev-21-1650/ where it says "42 counts per rev."
+    public static final double RevNeoEncoderTicksPerRevolution = 42;
+
+    // *****************************************************************
+    // ball handler system constants
     public static final int BallHandlerMotorCanId = 13;
     public static final double BallHandlerMotorDefaultSpeed = 1.0;
     public static final double BallHandlerMotorStorageDirectionMultiplier = 1.0;
@@ -74,7 +85,15 @@ public final class Constants {
     public static final int PneumaticsControlModuleForwardChannel = 0;
     public static final int PneumaticsControlModuleReverseChannel = 1;
 
+    // *****************************************************************
+    // input device constants
     public static final int portDriverController = 1;
     public static final int portCoDriverController = 2;
 
+    // *****************************************************************
+    // telescoping arm system constants
+    public static final int TelescopingArmMotorCanId = 14;
+    public static final double TelescopingArmDefaultExtendSpeed = 0.7;
+    public static final double TelescopingArmDefaultRetractSpeed = -0.7;
+    public static final double TelescopingArmStopSpeed = 0.0;    
 }
