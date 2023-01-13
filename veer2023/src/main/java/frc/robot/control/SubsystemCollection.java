6813234b5755136c57c036124e3fb3518966274a
josieammer <josieammer@gmail.com>
2023-01-12 16:44:43 -0800

    port Veer code to WPIlib 2023
    port Veer code to WPIlib 2023

---
+++ veer2023/src/main/java/frc/robot/control/SubsystemCollection.java
@@ -0,0 +1,40 @@
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - Rapid React - 2022
+// File: TelescopingArm.java
+// Intent: Forms a container that stores references to the current subsystems.
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
+
+package frc.robot.control;
+import frc.robot.subsystems.*;
+
+public class SubsystemCollection
+{
+    // declaring input classes
+    private ManualInputInterfaces manualInput = null;
+
+    // declaring and init subsystems  
+    private BallHandler ballHandler = null;
+    private DrivetrainSubsystem driveTrainSubsystem = null;
+    private TelescopingArm telescopingArm = null;
+
+    /**
+     * Default constructor
+     */
+    public SubsystemCollection() {}
+
+    public BallHandler getBallHandlerSubsystem() { return ballHandler; }
+    public void setBallHandlerSubsystem(BallHandler value) { ballHandler = value; }
+
+    public DrivetrainSubsystem getDriveTrainSubsystem() { return driveTrainSubsystem; }
+    public void setDriveTrainSubsystem(DrivetrainSubsystem value) { driveTrainSubsystem = value; }
+
+    public ManualInputInterfaces getManualInputInterfaces() { return manualInput; }
+    public void setManualInputInterfaces(ManualInputInterfaces value) { manualInput = value; }
+
+    public TelescopingArm getTelescopingArmSubsystem() { return telescopingArm; }
+    public void setTelescopingArmSubsystem(TelescopingArm value) { telescopingArm = value; }
+}
