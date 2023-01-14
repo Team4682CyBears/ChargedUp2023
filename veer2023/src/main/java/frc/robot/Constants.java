937544f525dbf524e3b2719262380f9fcff4f67a
Mike McAdams <56608471+mmcadamsms@users.noreply.github.com>
2023-01-14 09:16:29 -0800

    Migrate back to code in veer2023

--- veer2023/src/main/java/frc/robot/Constants.java
+++ veer2023/src/main/java/frc/robot/Constants.java
@@ -1,8 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - Rapid React - 2022
+// File: BallHandler.java
+// Intent: Wrapper class standard stub for robot in FRC challange.
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
 
 package frc.robot;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -13,7 +21,76 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+
+    /**
+     * The left-to-right distance between the drivetrain wheels
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.57785; 
+    /**
+     * The front-to-back distance between the drivetrain wheels.
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.57785; 
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; 
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; 
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 3; 
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(-353.4-180.0-90.0); 
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 4; 
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5; 
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 6; 
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(-321.0-180.0-110.0); 
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7; 
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8; 
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9; 
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(-205.5-180.0+10.0); 
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 10; 
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 11; 
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12; 
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(-305.5-180.0-117.0); 
+
+    // *****************************************************************
+    // standard stuff constants - motors rotation, etc.
+    public static final double DegreesPerRevolution = 360.0;
+    // NEO maximum RPM 
+    public static final double neoMaximumRevolutionsPerMinute = 5676;
+    // this uses the halls effect sensor when plugged into the spark max
+    // see: https://www.revrobotics.com/rev-21-1650/ where it says "42 counts per rev."
+    public static final double RevNeoEncoderTicksPerRevolution = 42;
+
+    // *****************************************************************
+    // ball handler system constants
+    public static final int BallHandlerMotorCanId = 15;
+    public static final double BallHandlerMotorDefaultSpeed = 1.0;
+    public static final double BallHandlerMotorStorageDirectionMultiplier = 1.0;
+    public static final double BallHandlerMotorRetrievalDirectionMultiplier = -1.0;
+    public static final double BallHandlerMotorMinimumAbsoluteInputValue = 0.1;
+    public static final boolean BallHandlerMotorInvertedDirection = false;
+    public static final double BallHandlerPneumaticsDeployCycleTimeSeconds = 0.5;
+    public static final double BallHandlerPneumaticsRetractCycleTimeSeconds = 0.5;
+    public static final double BallHandlerIntakeBallTimeSeconds = 2.0;
+    public static final double BallHandlerLayupBallTimeSeconds = 1.0;
+
+    public static final PneumaticsModuleType BallHandlerPneumaticsControlModuleType = PneumaticsModuleType.REVPH;
+    public static final int PneumaticsControlModuleNumber = 1;
+    public static final int PneumaticsControlModuleForwardChannel = 0;
+    public static final int PneumaticsControlModuleReverseChannel = 7;
+
+    // *****************************************************************
+    // input device constants
+    public static final int portDriverController = 0;
+    public static final int portCoDriverController = 1;
+
+    // *****************************************************************
+    // telescoping arm system constants
+    public static final int TelescopingArmMotorCanId = 14;
+    public static final double TelescopingArmDefaultExtendSpeed = 1.0;
+    public static final double TelescopingArmDefaultRetractSpeed = -1.0;
+    public static final double TelescopingArmStopSpeed = 0.0;    
 }
--- veer2023/src/main/java/frc/robot/RobotContainer.java
+++ veer2023/src/main/java/frc/robot/RobotContainer.java
@@ -4,51 +4,122 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.commands.DriveTimeCommand;
+import frc.robot.control.InstalledHardware;
+import frc.robot.control.ManualInputInterfaces;
+import frc.robot.control.SubsystemCollection;
+import frc.robot.subsystems.BallHandler;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.TelescopingArm;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+  private SubsystemCollection subsystems = new SubsystemCollection();
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * The container for the robot. Contains subsystems, OI devices, and commands.
    */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+  public RobotContainer() {
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // init the input system 
+    this.initializeManualInputInterfaces();
+
+    // init the various subsystems
+    this.initializeBallHandler();
+    this.initializeDrivetrainSubsystem();
+    this.initializeTelescopingArm();
+
+    // Configure the button bindings
+    this.subsystems.getManualInputInterfaces().initializeButtonCommandBindings();
+  }
+
+  private void initializeManualInputInterfaces()
+  {
+    if(InstalledHardware.coDriverXboxControllerInstalled &&
+      InstalledHardware.driverXboxControllerInstalled)
+    {
+      subsystems.setManualInputInterfaces(new ManualInputInterfaces(subsystems));
+      System.out.println("SUCCESS: initializeManualInputInterfaces");
+    }
+    else
+    {
+      System.out.println("FAIL: initializeManualInputInterfaces");
+    }
+  }
+
+  private void initializeBallHandler()
+  {
+    if(InstalledHardware.ballHandlingMotorInstalled && 
+      InstalledHardware.ballHandlingPneumaticsInstalled && 
+      InstalledHardware.compressorInstalled)
+    {
+      subsystems.setBallHandlerSubsystem(new BallHandler());
+      System.out.println("SUCCESS: initializeBallHandler");
+
+      subsystems.getBallHandlerSubsystem().setDefaultCommand(
+        new RunCommand(
+          () ->
+          subsystems.getBallHandlerSubsystem().setBallMotor(
+            subsystems.getManualInputInterfaces().getBallMotorSpeed()
+            ),
+            subsystems.getBallHandlerSubsystem()));
+    }
+    else
+    {
+      System.out.println("FAIL: initializeBallHandler");
+    }
+  }
+
+  private void initializeDrivetrainSubsystem()
+  {
+    if(InstalledHardware.leftFrontDriveInstalled && 
+      InstalledHardware.leftRearDriveInstalled && 
+      InstalledHardware.rightFrontDriveInstalled &&
+      InstalledHardware.rightRearDriveInstalled)
+    {
+      // The robot's subsystems and commands are defined here...
+      subsystems.setDriveTrainSubsystem(new DrivetrainSubsystem());
+      System.out.println("SUCCESS: initializeDrivetrain");
+
+      // Set up the default command for the drivetrain.
+      // The controls are for field-oriented driving:
+      // Left stick Y axis -> forward and backwards movement
+      // Left stick X axis -> left and right movement
+      // Right stick X axis -> rotation
+      subsystems.getDriveTrainSubsystem().setDefaultCommand(new DefaultDriveCommand(
+        subsystems.getDriveTrainSubsystem(),
+        () -> -modifyAxis(subsystems.getManualInputInterfaces().getInputArcadeDriveY()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        () -> -modifyAxis(subsystems.getManualInputInterfaces().getInputArcadeDriveX()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        () -> -modifyAxis(subsystems.getManualInputInterfaces().getInputSpinDriveX()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+      ));
+    }
+    else
+    {
+      System.out.println("FAIL: initializeDrivetrain");
+    }
+  }
+
+  private void initializeTelescopingArm()
+  {
+    if(InstalledHardware.telescopingArmsDriveMotorInstalled)
+    {
+      subsystems.setTelescopingArmSubsystem(new TelescopingArm());
+      System.out.println("SUCCESS: initializeTelescopingArm");
+    }
+    else
+    {
+      System.out.println("FAIL: initializeTelescopingArm");
+    }
   }
 
   /**
@@ -57,7 +128,29 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    // robot-centric drive command.  
+    return new DriveTimeCommand(subsystems.getDriveTrainSubsystem(), 0.4, 0.0, 0.0, 10.0);
+  }
+
+  private static double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+  private static double modifyAxis(double value) {
+    // Deadband
+    value = deadband(value, 0.05);
+
+    // Square the axis
+    value = Math.copySign(value * value, value);
+
+    return value;
   }
 }
