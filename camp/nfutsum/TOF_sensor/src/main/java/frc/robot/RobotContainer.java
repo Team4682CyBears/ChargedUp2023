4d22fa80b6da64630356246286eb914cd4a61a19
NaherIsCool <nfutsum.2025@bishopblanchet.org>
2023-02-01 16:06:46 -0800

    Fixed errors
    Fixed errors

--- camp/nfutsum/TOF_sensor/src/main/java/frc/robot/RobotContainer.java
+++ camp/nfutsum/TOF_sensor/src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,13 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ProximitySubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+//import frc.robot.Constants.OperatorConstants;
+//import frc.robot.commands.Autos;
+//import frc.robot.commands.ExampleCommand;
+//import frc.robot.subsystems.ProximitySubsystem;
+//import edu.wpi.first.wpilibj2.command.Command;
+//import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+//import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,16 +18,16 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-public class RobotContainer {
+/*public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ProximitySubsystem m_exampleSubsystem = new ProximitySubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
+  /*private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
+  /*public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
@@ -41,7 +41,7 @@ public class RobotContainer {
    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
-  private void configureBindings() {
+  /*private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
@@ -56,8 +56,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
+  /*public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     return Autos.exampleAuto(m_exampleSubsystem);
   }
-}
+}*/
--- camp/nfutsum/TOF_sensor/src/main/java/frc/robot/subsystems/ProximitySubsystem.java
+++ camp/nfutsum/TOF_sensor/src/main/java/frc/robot/subsystems/ProximitySubsystem.java
@@ -5,68 +5,33 @@
 package frc.robot.subsystems;
 
 import com.playingwithfusion.TimeOfFlight;
+import com.playingwithfusion.TimeOfFlight.RangingMode;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.util.Units;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class ProximitySubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  
+
+  // put CAN ID in constants.java
   public static TimeOfFlight tofSensor = new TimeOfFlight(0);
 
+  public void ProximitySubysytem(){
+    tofSensor.setRangingMode(RangingMode.Short, 200);
+  }
   
 
-public static final double getRange(){
+public double getRange(){
   //gets range and converts from millimeters to inches
-   return getRange(1000 * tofSensor.TimeOfFlight.RangingMode.Short * 39.37, 200.0);
+   return Units.metersToInches(1000 * tofSensor.getRange());
 }
 
 public static final double getRangeSigma(){
-  return  getRangeSigma(1000 * tofSensor.TimeOfFlight.RangingMode.Short * 39.37, 200.0);
+  return  tofSensor.getRangeSigma();
 }
 
 public boolean isRangeValid(){
-  if(isRangeValid()){
-    return true;
-  }else{
-    return false;
-  }
-
+  return tofSensor.isRangeValid();
 }
-
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public CommandBase exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
