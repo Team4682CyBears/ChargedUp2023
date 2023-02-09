99a8fb1c214ea62e381b0e50417c8e40ff8fd02d
Mike McAdams <mcadamswa@hotmail.com>
2023-02-09 15:37:33 -0800

    merge latest auto2

--- smokey/src/main/java/frc/robot/control/InstalledHardware.java
+++ smokey/src/main/java/frc/robot/control/InstalledHardware.java
@@ -45,6 +45,7 @@ public class InstalledHardware
 
     // Controller button install control
     public static final boolean applyBasicDriveToPointButtonsToDriverXboxController = false;
-    public static final boolean applyDriveTrajectoryButtonsToDriverXboxController = true;
+    public static final boolean applyDriveTrajectoryButtonsToDriverXboxController = false;
     public static final boolean applyDriveZeroPositionButtonToDriverXboxController = true;
+    public static final boolean applyAutoBalanceButtonToDriverXboxController = true;
 }
--- smokey/src/main/java/frc/robot/control/ManualInputInterfaces.java
+++ smokey/src/main/java/frc/robot/control/ManualInputInterfaces.java
@@ -21,6 +21,7 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.*;
 import frc.robot.commands.AutoBalanceCommand;
