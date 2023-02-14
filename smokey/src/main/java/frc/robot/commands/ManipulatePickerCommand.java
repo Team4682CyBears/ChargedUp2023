9b2bd9b7ec3b9416fd5f0eaebcad6d406e4b71e2
asherbolotski <70773941+asherbolotski@users.noreply.github.com>
2023-02-14 15:21:25 -0800

    first iteration
    first iteration

---
+++ smokey/src/main/java/frc/robot/commands/ManipulatePickerCommand.java
@@ -0,0 +1,40 @@
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - Charged Up - 2023
+// File: ManipulatePickerCommand.java
+// Intent: Forms a command to open or close the picker.
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ManipulatePickerCommand extends CommandBase {
+
+    /**
+     * Constructor to cause all subsystems to halt movements
+     * @param collection - the collection of subsystems
+     */
+    public ManipulatePickerCommand(Boolean isOpen) {
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished(){
+        return true;
+    }
+}
---
+++ smokey/src/main/java/frc/robot/commands/MoveArmToScoring.java
@@ -0,0 +1,40 @@
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - Charged Up - 2023
+// File: MoveArmToScoring.java
+// Intent: Forms a command to bring the arm to an inputed scoring position.
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class MoveArmToScoring extends CommandBase {
+
+    /**
+     * Constructor to cause all subsystems to halt movements
+     * @param collection - the collection of subsystems
+     */
+    public MoveArmToScoring(Enum postition) {
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished(){
+        return true;
+    }
+}
