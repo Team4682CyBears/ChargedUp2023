fe6a1edc4c40e52e2c22ad372e335c1c933c7c82
Mike McAdams <mcadamswa@hotmail.com>
2023-03-04 13:00:42 -0800

    Update Constants.java

--- smokey/src/main/java/frc/robot/Constants.java
+++ smokey/src/main/java/frc/robot/Constants.java
@@ -94,8 +94,8 @@ public final class Constants {
     // arm constants - preset positions
     public static final double armPresetPositionStowMetersY = 0.470;
     public static final double armPresetPositionStowMetersZ = 1.050;
-    public static final double armPresetPositionGrabMetersY = 0.698;
-    public static final double armPresetPositionGrabMetersZ = 0.900;
+    public static final double armPresetPositionGrabMetersY = 0.7736;
+    public static final double armPresetPositionGrabMetersZ = 0.8961;
     public static final double armPresetPositionScoreHighMetersY = 1.368;
     public static final double armPresetPositionScoreHighMetersZ = 1.089;
     public static final double armPresetPositionScoreMediumMetersY = 0.951;
