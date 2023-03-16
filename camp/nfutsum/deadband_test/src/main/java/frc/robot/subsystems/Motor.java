bd0cb1c3f661468741c231118b166ab509b467cc
NaherIsCool <nfutsum.2025@bishopblanchet.org>
2023-03-15 19:55:46 -0700

    tested deadband for xbox controller joystick

---
+++ camp/nfutsum/deadband_test/src/main/java/frc/robot/subsystems/Motor.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+
+
+
+public class Motor extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  private final CANSparkMax motor = new CANSparkMax(0, MotorType.kBrushless);
+
+  public void forward()
+  {
+    motor.set(1.0);
+  }
+  public void backward()
+  {
+    motor.set(-1.0);
+  }
+  public void stop()
+  {
+    motor.set(0.0);
+  }
+
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
