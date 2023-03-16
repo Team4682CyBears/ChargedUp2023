2fcbc31a99b21a045863b5058035ba8888816af4
josieammer <josieammer@gmail.com>
2023-03-16 11:20:54 -0700

    update DrivetrainSubsystem to only move to immovable_stance when chassis is not moving
    update DrivetrainSubsystem to only move to immovable_stance when chassis is not moving

--- smokey/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ smokey/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -412,7 +412,9 @@ public class DrivetrainSubsystem extends SubsystemBase {
     this.displayDiagnostics();
 
     SwerveModuleState[] states; 
-    if (swerveDriveMode == SwerveDriveMode.IMMOVABLE_STANCE) {
+    if (swerveDriveMode == SwerveDriveMode.IMMOVABLE_STANCE && chassisSpeedsAreZero()) {
+      // only change to ImmovableStance if chassis is not moving.
+      // otherwise, we could tip the robot moving to this stance when bot is at high velocity
       states = getImmovableStanceStates();
     }
     else { // SwerveDriveMode.NORMAL_DRIVING
@@ -481,9 +483,8 @@ public class DrivetrainSubsystem extends SubsystemBase {
    */
   public void setSwerveDriveMode(SwerveDriveMode swerveDriveMode) {
     this.swerveDriveMode = swerveDriveMode;
-    if (swerveDriveMode == SwerveDriveMode.IMMOVABLE_STANCE) {
-      this.chassisSpeeds = new ChassisSpeeds();
-    }
+    // if (swerveDriveMode == SwerveDriveMode.IMMOVABLE_STANCE) DO NOT set chassis speeds to 0.0 here.
+    // we need to allow the robot to decel properly so that it doesn't tip.  
   }
 
   /**
@@ -532,6 +533,12 @@ public class DrivetrainSubsystem extends SubsystemBase {
     return levelChecker;
   }
 
+  private boolean chassisSpeedsAreZero(){
+    return (chassisSpeeds.vxMetersPerSecond == 0.0) 
+    && (chassisSpeeds.vyMetersPerSecond == 0.0) 
+    && (chassisSpeeds.omegaRadiansPerSecond == 0.0);
+  }
+  
   /**
    * Clamps the chassis speeds between a min and max.  
    * Separete min and max for translational (x,y) vs. rotational speeds
