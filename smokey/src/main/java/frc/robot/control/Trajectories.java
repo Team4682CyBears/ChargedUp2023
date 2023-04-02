25fa7e530122afd4e4277028c5cf039a980385cd
josieammer <josieammer@gmail.com>
2023-04-01 20:42:18 -0700

    go sideways onto ramp to speed up node5 routine
    go sideways onto ramp to speed up node5 routine

--- smokey/src/main/java/frc/robot/control/Trajectories.java
+++ smokey/src/main/java/frc/robot/control/Trajectories.java
@@ -65,19 +65,19 @@ public class Trajectories {
         this.Node5Position = new Pose2d(1.678, 2.750, Rotation2d.fromDegrees(180));
         this.Node8Position = new Pose2d(1.678, 1.067, Rotation2d.fromDegrees(180));
         this.Node9Position = new Pose2d(1.678, 0.506, Rotation2d.fromDegrees(180));
-        this.InfrontOfRampPosition = new Pose2d(2.0, 2.41, //2.75
-        Rotation2d.fromDegrees(0)); //90
+        this.InfrontOfRampPosition = new Pose2d(2.0, 2.41, Rotation2d.fromDegrees(90)); 
         
-        // we need two different ramp waypoints.  There is slippage getting onto ramp, so 
-        // we need to overshoot the center to ensure the robot gets far enough onto the ramp.   
-        this.RampFarWaypoint = new Pose2d(4.122, 2.41, //2.748
-        Rotation2d.fromDegrees(0)); //90
-        Pose2d RampNearWaypoint = new Pose2d(3.34, 2.41, Rotation2d.fromDegrees(180));
-        Pose2d MiddlePathRampNearWaypoint = new Pose2d(4.0, 2.41, Rotation2d.fromDegrees(90));
-        // behind ramp position for node 1,2,8,9 paths
-        Pose2d BehindTrajectoryEndPosition = new Pose2d(5.27, 2.41, Rotation2d.fromDegrees(180));
+        // There is slippage getting onto ramp, so we need to overshoot the center 
+        // to ensure the robot gets far enough onto the ramp.   
+        this.RampFarWaypoint = new Pose2d(4.122, 2.41, Rotation2d.fromDegrees(90)); 
+
         // behind ramp position for node 5 path
         Pose2d MiddlePathOverRampPosition = new Pose2d(6.5, 2.41, Rotation2d.fromDegrees(90));
+        Pose2d MiddlePathRampNearWaypoint = new Pose2d(4.0, 2.41, Rotation2d.fromDegrees(90));
+        
+        Pose2d RampNearWaypoint = new Pose2d(3.34, 2.41, Rotation2d.fromDegrees(180));
+        // behind ramp position for node 1,2,8,9 paths
+        Pose2d BehindTrajectoryEndPosition = new Pose2d(5.27, 2.41, Rotation2d.fromDegrees(180));
         
         // Left waypoints drive from Node 1 or 2 to a location out of the community
         ArrayList<Translation2d> LeftWaypoints = new ArrayList<Translation2d>();
