9fce36e620fcfe5c14312a9bb7f5530e4e7e7335
Mike McAdams <mcadamswa@hotmail.com>
2023-04-06 08:44:09 -0700

    pick and place heights changes

--- smokey/src/main/java/frc/robot/Constants.java
+++ smokey/src/main/java/frc/robot/Constants.java
@@ -104,21 +104,21 @@ public final class Constants {
     public static final double armPresetPositionStowMetersHorizontalExtension = 0.0;
     public static final double armPresetPositionStowMetersVerticalExtension = 0.1952;
 
-    public static final double armPresetPositionCubeGrabMetersHorizontalExtension = 0.3041 - Units.inchesToMeters(1.5);
+    public static final double armPresetPositionCubeGrabMetersHorizontalExtension = 0.3041 - Units.inchesToMeters(0);
     public static final double armPresetPositionCubeGrabMetersVerticalExtension = 0.0691 - Units.inchesToMeters(1.5);
     public static final double armPresetPositionCubeScoreHighMetersHorizontalExtension = 0.7262;
     public static final double armPresetPositionCubeScoreHighMetersVerticalExtension = 0.0058;
     public static final double armPresetPositionCubeScoreMediumMetersHorizontalExtension = 0.1880;
-    public static final double armPresetPositionCubeScoreMediumMetersVerticalExtension = 0.0308;
+    public static final double armPresetPositionCubeScoreMediumMetersVerticalExtension = 0.0158;
     public static final double armPresetPositionCubeScoreLowMetersHorizontalExtension = 0.0979;
     public static final double armPresetPositionCubeScoreLowMetersVerticalExtension = 0.0224;
 
-    public static final double armPresetPositionConeGrabMetersHorizontalExtension = 0.4192 - Units.inchesToMeters(1.5);
+    public static final double armPresetPositionConeGrabMetersHorizontalExtension = 0.4192 - Units.inchesToMeters(0);
     public static final double armPresetPositionConeGrabMetersVerticalExtension = 0.0589 - Units.inchesToMeters(1.5);
     public static final double armPresetPositionConeScoreHighMetersHorizontalExtension = 0.6694;
-    public static final double armPresetPositionConeScoreHighMetersVerticalExtension = 0.0672;
-    public static final double armPresetPositionConeScoreMediumMetersHorizontalExtension = 0.2230;
-    public static final double armPresetPositionConeScoreMediumMetersVerticalExtension = 0.0375;
+    public static final double armPresetPositionConeScoreHighMetersVerticalExtension = 0.0;
+    public static final double armPresetPositionConeScoreMediumMetersHorizontalExtension = 0.2 - Units.inchesToMeters(1);
+    public static final double armPresetPositionConeScoreMediumMetersVerticalExtension = 0.01;
     public static final double armPresetPositionConeScoreLowMetersHorizontalExtension = 0.0979;
     public static final double armPresetPositionConeScoreLowMetersVerticalExtension = 0.0224;
 
