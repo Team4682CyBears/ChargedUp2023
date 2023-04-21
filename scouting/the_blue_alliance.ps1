ad6292c11a9816c961007d1c0980d277d74eb127
Mike McAdams <56608471+mmcadamsms@users.noreply.github.com>
2023-04-20 20:08:56 -0700

    match analysis at worlds

---
+++ scouting/the_blue_alliance.ps1
@@ -0,0 +1,37 @@
+﻿
+$blueAllianceApiBase = "https://www.thebluealliance.com/api/v3"
+$authKey = "F9XCNPPfm6yEHD3s4hqTrzBEbZnHbnSq37xXmRCDKv3Si2l61gLISHOvt7CMNTaJ"
+
+$headers = @{
+    'X-TBA-Auth-Key'=$authKey
+    Content='application/json'
+}
+
+$eventsApi = $blueAllianceApiBase + "/events/2023"
+
+$eventResults = Invoke-RestMethod -Method Get -Uri $eventsApi -Headers $headers
+
+ForEach($nextEvent in $eventResults)
+{
+    if($nextEvent.city -eq "Houston" -and $nextEvent.event_type -eq 3)
+    {
+#       Write-Host "**********************************************"
+#       Write-Host $nextEvent.name
+
+       $matchesApi = $blueAllianceApiBase + "/event/" + $nextEvent.key + "/matches"
+
+       $matches = Invoke-RestMethod -Method Get -Uri $matchesApi -Headers $headers
+       ForEach($nextMatch in $matches)
+       {
+           $totalScore = $nextMatch.alliances.blue.score + $nextMatch.alliances.red.score
+           if($totalScore -gt -1)
+           {
+                $lineToLog =  "" + $nextMatch.match_number + "_" + $nextEvent.key + ", " + $nextMatch.alliances.blue.score + ", " + $nextMatch.alliances.red.score + ", " + $totalScore
+                Write-Output $lineToLog
+           }
+       }
+
+    }
+}
+
+Write-Host ""
