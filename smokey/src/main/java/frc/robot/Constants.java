748ab280ef7fe94b75516079e17fa2ae91013224
Mike McAdams <mcadamswa@hotmail.com>
2023-10-11 16:48:31 -0700

    change yaw acceleration
    change yaw acceleration

--- smokey/src/main/java/frc/robot/Constants.java
+++ smokey/src/main/java/frc/robot/Constants.java
@@ -151,7 +151,7 @@ public final class Constants {
     // PowerDistributionPanel Constants
     public static final int currentPowerDistributionPanelCanId = 19;
     public static final ModuleType currentPowerDistributionPanelType = ModuleType.kRev;
-    public static final double overcurrentRumbleTimeSeconds = 0.25;
+    public static final double overcurrentRumbleTimeSeconds = 0.1;
     public static final int EveryBotMotorPdpPortId = 8;
     public static final double EveryBotMotorMaximuCurrentAmps = 45.5;
 
