bce406880a642571856e491a11f13704c8ae10bd
Mike McAdams <mcadamswa@hotmail.com>
2023-11-29 15:23:51 -0800

    Update CameraSubsystem.java

--- smokey/src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ smokey/src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -12,12 +12,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import pabeles.concurrency.ConcurrencyOps.NewInstance;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.math.MathSharedStore;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -27,30 +24,10 @@ public class CameraSubsystem extends SubsystemBase {
   DrivetrainSubsystem drivetrainSubsystem;
   double storedtid = -1;
 
-  /** Creates a new ExampleSubsystem. */
+  /** Creates a new CameraSubsystem. */
   public CameraSubsystem(SubsystemCollection subsystems) {
     drivetrainSubsystem = subsystems.getDriveTrainSubsystem();
   }
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public CommandBase exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
 
   @Override
   public void periodic() {
@@ -62,19 +39,9 @@ public class CameraSubsystem extends SubsystemBase {
     Rotation2d botYaw = new Rotation2d(botpose[5]);
     Pose2d RealBotpos = new Pose2d(botTranslation, botYaw);
 
-    //drivetrainSubsystem.AddVisionMeasurement(RealBotpos, timestamp);
-
     if (tid != -1){
-      Pose2d prevPose = drivetrainSubsystem.getRobotPosition();
       drivetrainSubsystem.AddVisionMeasurement(RealBotpos, timestamp);
-      Pose2d newPose = drivetrainSubsystem.getRobotPosition();
     }
-  }
-
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
 
